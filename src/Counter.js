5fa42d50900aa36f0b629eed3fd40cbc06512f90
ahmedsalah674 <ahmad.mohamadsalah@gmail.com>
2022-04-11 14:45:59 +0200

    lab1

---
+++ src/Counter.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+import { AiFillPlusCircle, AiFillMinusCircle } from "react-icons/ai";
+function Counter(props) {
+    let [counter, setCounter] = useState(props.value)
+
+    const increaseCounter = () => {
+        setCounter(++counter);
+    }
+    const decreaseCounter = () => {
+        setCounter(--counter);
+    }
+    return (
+        <div>
+            <h1>
+                {counter}
+            </h1>
+            <button className='btn btn-primary mx-2' onClick={increaseCounter}><AiFillPlusCircle /></button>
+            <button className='btn btn-danger mx-2' onClick={decreaseCounter}><AiFillMinusCircle /></button>
+
+        </div>
+    )
+}
+
+export default Counter
